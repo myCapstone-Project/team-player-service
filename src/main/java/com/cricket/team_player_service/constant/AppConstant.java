27a1312c3e2be38137039c5ffd8d45844a83cb56
akhilaPuppala <akhilandeshwariv@gmail.com>
2025-02-02 11:02:47 +0530

    updated

---
+++ src/main/java/com/cricket/team_player_service/constant/AppConstant.java
@@ -0,0 +1,7 @@
+package com.cricket.team_player_service.constant;
+
+public class AppConstant {
+	
+	public static final String TOPIC_DRIVER_LOCATION = "driver-location";
+
+}
--- src/main/java/com/cricket/team_player_service/controller/TeamPlayerController.java
+++ src/main/java/com/cricket/team_player_service/controller/TeamPlayerController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +20,7 @@ import com.cricket.team_player_service.service.TeamPlayersService;
 import lombok.extern.slf4j.Slf4j;
 @RestController
 @RequestMapping("/api/team-players")
+@CrossOrigin("*")
 	public class TeamPlayerController {
 
 	    @Autowired
