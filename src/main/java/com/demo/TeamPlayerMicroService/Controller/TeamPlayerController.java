dc590889edc8534e09457c7dff11c7f64d24b655
vishrutth5214 <vishrutth5214@gmail.com>
2025-02-03 17:33:28 +0530

    updated
    added inter service communication

--- src/main/java/com/demo/TeamPlayerMicroService/Controller/TeamPlayerController.java
+++ src/main/java/com/demo/TeamPlayerMicroService/Controller/TeamPlayerController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,18 +18,14 @@ import com.demo.TeamPlayerMicroService.Entity.TeamPlayer;
 import com.demo.TeamPlayerMicroService.Service.TeamPlayersService;
 
 import lombok.extern.slf4j.Slf4j;
-
-@RestController
-@RequestMapping("/api/teamplayers")
-@Slf4j
-public class TeamPlayerController {
-
+@CrossOrigin("*")
 	@RestController
 	@RequestMapping("/api/team-players")
-	public class TeamPlayersController {
+	public class TeamPlayerController {
 
 	    @Autowired
 	    private TeamPlayersService teamPlayersService;
+	    
 
 	    // Create or Update TeamPlayer
 	    @PostMapping
@@ -66,4 +63,4 @@ public class TeamPlayerController {
 	    }
 	}
 
-}
+
